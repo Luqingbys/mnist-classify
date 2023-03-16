c7b40efff14fec13cda73f2c3e6d7a8d30eb3ea8
Luqingbys <hit_xiaohanbin@163.com>
2023-03-16 18:40:14 +0800

    添加模型评估、图像归一化，提高测试集准确率

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def train(net: nn.Module, dataLoader, optimizer, lossFunc, output, device='cpu')
     '''
     logger = get_logger(path=output, filename='train.log')
     logger.info('Start trainning......')
-    EPOCHS = 50
+    EPOCHS = 5
     # 存储训练过程
     history = {'Train Loss': [], 'Train Accuracy': []}
     for epoch in range(1, EPOCHS + 1):
@@ -29,6 +29,9 @@ def train(net: nn.Module, dataLoader, optimizer, lossFunc, output, device='cpu')
 
             trainImgs = trainImgs.to(device)
             labels = labels.to(device)
+
+            # m, s = trainImgs.mean(), trainImgs.std()
+            # trainImgs = (trainImgs - m) / s
 
             net.zero_grad()
             outputs = net(trainImgs)
@@ -43,9 +46,13 @@ def train(net: nn.Module, dataLoader, optimizer, lossFunc, output, device='cpu')
             processBar.set_description("[%d/%d] Loss: %.4f, Acc: %.4f" %
                                     (epoch, EPOCHS, loss.item(), accuracy.item()))
             
+            if step % 1000 == 0:
+                history['Train Loss'].append(loss.item())
+                history['Train Accuracy'].append(accuracy.item())
+
         logger.info('Epoch:[{}/{}]\t loss={:.5f}\t acc={:.3f}'.format(epoch , EPOCHS, loss, accuracy))
-        # history['Train Loss'].append(loss.item())
-        # history['Train Accuracy'].append(accuracy.item())
+        history['Train Loss'].append(loss.item())
+        history['Train Accuracy'].append(accuracy.item())
         processBar.close()
     
     logger.info('Finish training!')
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,13 +1,16 @@
 from torch.utils.data import DataLoader, Dataset, random_split
 import torch
+from torchvision import transforms
 import numpy as np
 from PIL import Image
 
 
 class sampleDataset(Dataset):
-    def __init__(self, df) -> None:
+    def __init__(self, df, use_transform=True) -> None:
         super().__init__()
         self.df = df
+        if use_transform:
+            self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5], std=[0.5])])
 
     
     def __len__(self):
@@ -17,7 +20,10 @@ class sampleDataset(Dataset):
     def __getitem__(self, index):
         path = 'data/MNIST/png/'
         '''获取第index份图像数据'''
-        file = self.df.loc[index]['path']
-        class_id = self.df.loc[index]['label']
-        image = np.array(Image.open(path+file)) # image: (28, 28)
-        return torch.tensor(image, dtype=torch.float).unsqueeze(0), int(class_id) # (1, 28, 28)
+        file = self.df.loc[index][0]
+        class_id = self.df.loc[index][1]
+        image = Image.open(path+file)
+        if self.transform != None:
+            image = self.transform(image) # image: Tensor(1, 28, 28)
+        return image, int(class_id)
+        # return torch.tensor(image, dtype=torch.float).unsqueeze(0), int(class_id) # (1, 28, 28)
